93eddc9069e4fe783f5ca5d6b48045556b69f315
Jesse Wilson <jwilson@squareup.com>
2014-08-10 13:20:27 -0400

    Initial JsonAdapter structure.
    This is similar to the way Gson does its type adapters: factories that
    can delegate and compose.
    
    It's different because annotations are fundamental to the design.
    
    It's also different because there are no APIs to convert to and from
    JSON on the central object: instead callers must get the JSON adapter
    they're interested in and do that there. This is potentially more
    efficient because applications can hold the adapter they need.

---
+++ moshi/src/main/java/com/squareup/moshi/JsonAdapter.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2014 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.squareup.moshi;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Type;
+
+/**
+ * Converts Java values to JSON, and JSON values to Java.
+ */
+public abstract class JsonAdapter<T> {
+  public abstract T fromJson(JsonReader reader) throws IOException;
+
+  public final T fromJson(String string) throws IOException {
+    return fromJson(new JsonReader(string));
+  }
+
+  public abstract void toJson(JsonWriter writer, T value) throws IOException;
+
+  public final String toJson(T value) throws IOException {
+    StringWriter stringWriter = new StringWriter();
+    toJson(new JsonWriter(stringWriter), value);
+    return stringWriter.toString();
+  }
+
+  /**
+   * Returns a JSON adapter equal to this JSON adapter, but with support for reading and writing
+   * nulls.
+   */
+  public final JsonAdapter<T> nullSafe() {
+    final JsonAdapter<T> delegate = this;
+    return new JsonAdapter<T>() {
+      @Override public T fromJson(JsonReader reader) throws IOException {
+        if (reader.peek() == JsonToken.NULL) {
+          return reader.nextNull();
+        } else {
+          return delegate.fromJson(reader);
+        }
+      }
+      @Override public void toJson(JsonWriter writer, T value) throws IOException {
+        if (value == null) {
+          writer.nullValue();
+        } else {
+          delegate.toJson(writer, value);
+        }
+      }
+    };
+  }
+
+  /** Returns a JSON adapter equal to this JSON adapter, but is lenient when reading and writing. */
+  public final JsonAdapter<T> lenient() {
+    final JsonAdapter<T> delegate = this;
+    return new JsonAdapter<T>() {
+      @Override public T fromJson(JsonReader reader) throws IOException {
+        boolean lenient = reader.isLenient();
+        reader.setLenient(true);
+        try {
+          return delegate.fromJson(reader);
+        } finally {
+          reader.setLenient(lenient);
+        }
+      }
+      @Override public void toJson(JsonWriter writer, T value) throws IOException {
+        boolean lenient = writer.isLenient();
+        writer.setLenient(true);
+        try {
+          delegate.toJson(writer, value);
+        } finally {
+          writer.setLenient(lenient);
+        }
+      }
+    };
+  }
+
+  public interface Factory {
+    /**
+     * Attempts to create an adapter for {@code type} annotated with {@code annotations}. This
+     * returns the adapter if one was created, or null if this factory isn't capable of creating
+     * such an adapter.
+     *
+     * <p>Implementations may use to {@link Moshi#adapter} to compose adapters of other types, or
+     * {@link Moshi#nextAdapter} to delegate to the underlying adapter of the same type.
+     */
+    JsonAdapter<?> create(Type type, AnnotatedElement annotations, Moshi moshi);
+  }
+}
