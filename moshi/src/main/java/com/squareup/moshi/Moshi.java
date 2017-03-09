27781f94280fea960fa48f0f1379cc8e3bfd9848
Christian <c.brueggemann94@gmail.com>
2017-03-09 17:51:19 +0100

    Revert style change

--- moshi/src/main/java/com/squareup/moshi/Moshi.java
+++ moshi/src/main/java/com/squareup/moshi/Moshi.java
@@ -18,7 +18,13 @@ package com.squareup.moshi;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Coordinates binding between JSON values and Java objects.
