5b788797b4aea32dc1b16279e0cc5d3aa0c5b572
cbruegg <mail@cbruegg.com>
2017-03-21 00:03:46 +0100

    Fix indent

--- kotlin/src/main/java/com/squareup/moshi/KotlinJsonAdapter.kt
+++ kotlin/src/main/java/com/squareup/moshi/KotlinJsonAdapter.kt
@@ -31,118 +31,118 @@ import kotlin.reflect.jvm.javaField
  * - If the class is a platform type (kotlin.*), it may not contain private fields.
  */
 class KotlinJsonAdapterFactory(private val crashOnMissingValues: Boolean = false) : JsonAdapter.Factory {
-    override fun create(type: Type, annotations: MutableSet<out Annotation>, moshi: Moshi): JsonAdapter<*>? {
-        val kclass = (type as? Class<*>)?.kotlin ?: return null
-        if (!kclass.isData) return null
-        // This gives us a few guarantees:
-        // 1. Primary constructor exists
-        // 2. Constructor params are var/val and have backing fields
-        // 3. Class is not abstract or an interface
-        // 4. Class is final
+  override fun create(type: Type, annotations: MutableSet<out Annotation>, moshi: Moshi): JsonAdapter<*>? {
+    val kclass = (type as? Class<*>)?.kotlin ?: return null
+    if (!kclass.isData) return null
+    // This gives us a few guarantees:
+    // 1. Primary constructor exists
+    // 2. Constructor params are var/val and have backing fields
+    // 3. Class is not abstract or an interface
+    // 4. Class is final
 
-        val constructor = kclass.primaryConstructor!!
-        constructor.isAccessible = true
+    val constructor = kclass.primaryConstructor!!
+    constructor.isAccessible = true
 
-        val concretePropertyByName = kclass.declaredMemberProperties
-                .filter { it.javaField != null }
-                .associateBy { it.name }
-        val constructorParameterByName = constructor.parameters.associateBy { it.name }
-        for ((name, property) in concretePropertyByName) {
-            if (name !in constructorParameterByName.keys &&
-                    !Modifier.isTransient(property.javaField!!.modifiers)) {
-                throw IllegalArgumentException("Property $name is not transient. " +
-                        "Any properties not found in the primary constructor need to be transient. " +
-                        "If this is a delegated property, mark the delegate as transient " +
-                        "using @delegate:Transient.")
-            }
-        }
-
-        constructor.parameters
-                .asSequence()
-                .filterNot { it.isOptional }
-                .map { concretePropertyByName[it.name]!! }
-                .firstOrNull { it.findAnnotation<Transient>() != null }
-                ?.run { throw IllegalArgumentException("Transient properties in primary constructor are unsupported.") }
-
-        val isPlatformType = isPlatformType(Types.getRawType(type))
-        val notIncludedParams = constructorParameterByName
-                .filterNot { it.value.isOptional }
-                .keys.map { concretePropertyByName[it]!! }
-                .filter { !includeField(isPlatformType, it.javaField!!.modifiers) }
-        if (notIncludedParams.isNotEmpty()) {
-            throw IllegalArgumentException("Primary constructor contains parameters " +
-                    "not found in serialized JSON: $notIncludedParams")
-        }
-
-        val toJsonAdapter = ClassJsonAdapter.FACTORY.create(type, annotations, moshi)
-        val propertyByParam = constructor.parameters.associate { it to concretePropertyByName[it.name]!! }
-        val adapterByParam = constructor.parameters.associate { param ->
-            val javaField = propertyByParam[param]!!.javaField!!
-            val paramType = Types.resolve(javaField.type, Types.getRawType(javaField.type), javaField.genericType)
-            val annotations = param.annotations.toSet()
-
-            param to moshi.adapter<Any>(paramType, annotations)
-        }
-
-        return KotlinJsonAdapter(
-                constructor,
-                toJsonAdapter as JsonAdapter<Any?>,
-                propertyByParam,
-                adapterByParam,
-                crashOnMissingValues
-        )
+    val concretePropertyByName = kclass.declaredMemberProperties
+        .filter { it.javaField != null }
+        .associateBy { it.name }
+    val constructorParameterByName = constructor.parameters.associateBy { it.name }
+    for ((name, property) in concretePropertyByName) {
+      if (name !in constructorParameterByName.keys &&
+          !Modifier.isTransient(property.javaField!!.modifiers)) {
+        throw IllegalArgumentException("Property $name is not transient. " +
+            "Any properties not found in the primary constructor need to be transient. " +
+            "If this is a delegated property, mark the delegate as transient " +
+            "using @delegate:Transient.")
+      }
     }
+
+    constructor.parameters
+        .asSequence()
+        .filterNot { it.isOptional }
+        .map { concretePropertyByName[it.name]!! }
+        .firstOrNull { it.findAnnotation<Transient>() != null }
+        ?.run { throw IllegalArgumentException("Transient properties in primary constructor are unsupported.") }
+
+    val isPlatformType = isPlatformType(Types.getRawType(type))
+    val notIncludedParams = constructorParameterByName
+        .filterNot { it.value.isOptional }
+        .keys.map { concretePropertyByName[it]!! }
+        .filter { !includeField(isPlatformType, it.javaField!!.modifiers) }
+    if (notIncludedParams.isNotEmpty()) {
+      throw IllegalArgumentException("Primary constructor contains parameters " +
+          "not found in serialized JSON: $notIncludedParams")
+    }
+
+    val toJsonAdapter = ClassJsonAdapter.FACTORY.create(type, annotations, moshi)
+    val propertyByParam = constructor.parameters.associate { it to concretePropertyByName[it.name]!! }
+    val adapterByParam = constructor.parameters.associate { param ->
+      val javaField = propertyByParam[param]!!.javaField!!
+      val paramType = Types.resolve(javaField.type, Types.getRawType(javaField.type), javaField.genericType)
+      val annotations = param.annotations.toSet()
+
+      param to moshi.adapter<Any>(paramType, annotations)
+    }
+
+    return KotlinJsonAdapter(
+        constructor,
+        toJsonAdapter as JsonAdapter<Any?>,
+        propertyByParam,
+        adapterByParam,
+        crashOnMissingValues
+    )
+  }
 
 }
 
 @Suppress("LoopToCallChain")
 internal class KotlinJsonAdapter<T>(
-        private val constructor: KFunction<T>,
-        private val toJsonAdapter: JsonAdapter<T>,
-        propertyByParam: Map<KParameter, KProperty1<out Any, Any?>>,
-        private val adapterByParam: Map<KParameter, JsonAdapter<*>>,
-        private val crashOnMissingValues: Boolean
+    private val constructor: KFunction<T>,
+    private val toJsonAdapter: JsonAdapter<T>,
+    propertyByParam: Map<KParameter, KProperty1<out Any, Any?>>,
+    private val adapterByParam: Map<KParameter, JsonAdapter<*>>,
+    private val crashOnMissingValues: Boolean
 ) : JsonAdapter<T>() {
 
-    private val jsonNames = constructor.parameters
-            .map { propertyByParam[it]!! }
-            .map { it.javaField!!.getAnnotation(Json::class.java)?.name ?: it.name }
-            .toTypedArray()
-    private val options = JsonReader.Options.of(*jsonNames)
+  private val jsonNames = constructor.parameters
+      .map { propertyByParam[it]!! }
+      .map { it.javaField!!.getAnnotation(Json::class.java)?.name ?: it.name }
+      .toTypedArray()
+  private val options = JsonReader.Options.of(*jsonNames)
 
-    override fun fromJson(reader: JsonReader): T {
-        try {
-            val valuesByParam = mutableMapOf<KParameter, Any?>()
+  override fun fromJson(reader: JsonReader): T {
+    try {
+      val valuesByParam = mutableMapOf<KParameter, Any?>()
 
-            reader.beginObject()
-            while (reader.hasNext()) {
-                val index = reader.selectName(options)
-                val param = if (index != -1) {
-                    constructor.parameters[index]
-                } else {
-                    reader.nextName()
-                    reader.skipValue()
-                    continue
-                }
-
-                valuesByParam[param] = adapterByParam[param]!!.fromJson(reader)
-            }
-            reader.endObject()
-
-            for (param in constructor.parameters) {
-                if ((crashOnMissingValues || !param.isOptional) && param !in valuesByParam) {
-                    throw IllegalArgumentException("JSON is missing value for ${param.name}")
-                }
-            }
-
-            return constructor.callBy(valuesByParam)
-        } catch (e: IllegalAccessException) {
-            throw AssertionError()
+      reader.beginObject()
+      while (reader.hasNext()) {
+        val index = reader.selectName(options)
+        val param = if (index != -1) {
+          constructor.parameters[index]
+        } else {
+          reader.nextName()
+          reader.skipValue()
+          continue
         }
-    }
 
-    override fun toJson(writer: JsonWriter, value: T) {
-        toJsonAdapter.toJson(writer, value)
+        valuesByParam[param] = adapterByParam[param]!!.fromJson(reader)
+      }
+      reader.endObject()
+
+      for (param in constructor.parameters) {
+        if ((crashOnMissingValues || !param.isOptional) && param !in valuesByParam) {
+          throw IllegalArgumentException("JSON is missing value for ${param.name}")
+        }
+      }
+
+      return constructor.callBy(valuesByParam)
+    } catch (e: IllegalAccessException) {
+      throw AssertionError()
     }
+  }
+
+  override fun toJson(writer: JsonWriter, value: T) {
+    toJsonAdapter.toJson(writer, value)
+  }
 
 }
