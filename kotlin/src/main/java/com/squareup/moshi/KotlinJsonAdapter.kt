ee338f4f8514984f191b607eaa860ee350bece85
cbruegg <mail@cbruegg.com>
2017-03-20 23:55:45 +0100

    Add kotlin-module with support for Kotlin data classes

---
+++ kotlin/src/main/java/com/squareup/moshi/KotlinJsonAdapter.kt
@@ -0,0 +1,148 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package com.squareup.moshi
+
+import com.squareup.moshi.ClassJsonAdapter.includeField
+import com.squareup.moshi.ClassJsonAdapter.isPlatformType
+import java.lang.reflect.Modifier
+import java.lang.reflect.Type
+import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.jvm.javaField
+
+/**
+ * A [JsonAdapter.Factory] for Kotlin data classes. If this is added to Moshi,
+ * data classes will be treated differently from other classes.
+ *
+ * Moshi will call its primary constructor to create the object. This allows for delegated properties
+ * to be initialized correctly. If the JSON does not contain a value for a parameter with a default value
+ * and [crashOnMissingValues] is set to false, the default value is used. If no default value is present,
+ * an [IllegalArgumentException] will be thrown during deserialization.
+ *
+ * Data classes to be (de-)serialized must conform to the following rules:
+ *
+ * - Any properties not found in the primary constructor must be transient, including delegated properties.
+ * - The primary constructor may not contain transient properties unless a default value is provided.
+ * - If the class is a platform type (kotlin.*), it may not contain private fields.
+ */
+class KotlinJsonAdapterFactory(private val crashOnMissingValues: Boolean = false) : JsonAdapter.Factory {
+    override fun create(type: Type, annotations: MutableSet<out Annotation>, moshi: Moshi): JsonAdapter<*>? {
+        val kclass = (type as? Class<*>)?.kotlin ?: return null
+        if (!kclass.isData) return null
+        // This gives us a few guarantees:
+        // 1. Primary constructor exists
+        // 2. Constructor params are var/val and have backing fields
+        // 3. Class is not abstract or an interface
+        // 4. Class is final
+
+        val constructor = kclass.primaryConstructor!!
+        constructor.isAccessible = true
+
+        val concretePropertyByName = kclass.declaredMemberProperties
+                .filter { it.javaField != null }
+                .associateBy { it.name }
+        val constructorParameterByName = constructor.parameters.associateBy { it.name }
+        for ((name, property) in concretePropertyByName) {
+            if (name !in constructorParameterByName.keys &&
+                    !Modifier.isTransient(property.javaField!!.modifiers)) {
+                throw IllegalArgumentException("Property $name is not transient. " +
+                        "Any properties not found in the primary constructor need to be transient. " +
+                        "If this is a delegated property, mark the delegate as transient " +
+                        "using @delegate:Transient.")
+            }
+        }
+
+        constructor.parameters
+                .asSequence()
+                .filterNot { it.isOptional }
+                .map { concretePropertyByName[it.name]!! }
+                .filter { it.findAnnotation<Transient>() != null }
+                .forEach { throw IllegalArgumentException("Transient properties in primary constructor are unsupported.") }
+
+        val isPlatformType = isPlatformType(Types.getRawType(type))
+        val notIncludedParams = constructorParameterByName
+                .filterNot { it.value.isOptional }
+                .keys.map { concretePropertyByName[it]!! }
+                .filter { !includeField(isPlatformType, it.javaField!!.modifiers) }
+        if (notIncludedParams.isNotEmpty()) {
+            throw IllegalArgumentException("Primary constructor contains parameters " +
+                    "not found in serialized JSON: $notIncludedParams")
+        }
+
+        val toJsonAdapter = ClassJsonAdapter.FACTORY.create(type, annotations, moshi)
+        val propertyByParam = constructor.parameters.associate { it to concretePropertyByName[it.name]!! }
+        val adapterByParam = constructor.parameters.associate { param ->
+            val javaField = propertyByParam[param]!!.javaField!!
+            val paramType = Types.resolve(javaField.type, Types.getRawType(javaField.type), javaField.genericType)
+            val annotations = param.annotations.toSet()
+
+            param to moshi.adapter<Any>(paramType, annotations)
+        }
+
+        return KotlinJsonAdapter(
+                constructor,
+                toJsonAdapter as JsonAdapter<Any?>,
+                propertyByParam,
+                adapterByParam,
+                crashOnMissingValues
+        )
+    }
+
+}
+
+@Suppress("LoopToCallChain")
+internal class KotlinJsonAdapter<T>(
+        private val constructor: KFunction<T>,
+        private val toJsonAdapter: JsonAdapter<T>,
+        propertyByParam: Map<KParameter, KProperty1<out Any, Any?>>,
+        private val adapterByParam: Map<KParameter, JsonAdapter<*>>,
+        private val crashOnMissingValues: Boolean
+) : JsonAdapter<T>() {
+
+    private val jsonNames = constructor.parameters
+            .map { propertyByParam[it]!! }
+            .map { it.javaField!!.getAnnotation(Json::class.java)?.name ?: it.name }
+            .toTypedArray()
+    private val options = JsonReader.Options.of(*jsonNames)
+
+    override fun fromJson(reader: JsonReader): T {
+        try {
+            val valuesByParam = mutableMapOf<KParameter, Any?>()
+
+            reader.beginObject()
+            while (reader.hasNext()) {
+                val index = reader.selectName(options)
+                val param = if (index != -1) {
+                    constructor.parameters[index]
+                } else {
+                    reader.nextName()
+                    reader.skipValue()
+                    continue
+                }
+
+                valuesByParam[param] = adapterByParam[param]!!.fromJson(reader)
+            }
+            reader.endObject()
+
+            for (param in constructor.parameters) {
+                if ((crashOnMissingValues || !param.isOptional) && param !in valuesByParam) {
+                    throw IllegalArgumentException("JSON is missing value for ${param.name}")
+                }
+            }
+
+            return constructor.callBy(valuesByParam)
+        } catch (e: IllegalAccessException) {
+            throw AssertionError()
+        }
+    }
+
+    override fun toJson(writer: JsonWriter, value: T) {
+        toJsonAdapter.toJson(writer, value)
+    }
+
+}
---
+++ kotlin/src/test/java/com/squareup/moshi/KotlinJsonAdapterTest.kt
@@ -0,0 +1,81 @@
+package com.squareup.moshi
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class KotlinJsonAdapterFactoryTest {
+
+    private val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    private data class FooBar(val foo: String, @Json(name = "other_name") val named: String) {
+        @Transient val otherProperty = ""
+        @delegate:Transient val delegated by lazy { "" }
+    }
+
+    @Test fun test() {
+        val adapter = moshi.adapter(FooBar::class.java)
+
+        val json = "{\"foo\":\"foo\", \"other_name\": \"other_name\"}"
+        val deserialized = adapter.fromJson(json)
+        assertEquals("foo", deserialized.foo, "val deserialization without @Json annotation failed.")
+        assertEquals("other_name", deserialized.named, "@Json(name = ...) was not respected.")
+        assertEquals("", deserialized.otherProperty, "Transient property was not initialized. " +
+                "Was the primary constructor called?")
+        assertEquals("", deserialized.delegated, "Delegated property was not initialized. " +
+                "Was the primary constructor called?")
+
+        assertEquals(deserialized, adapter.fromJson(adapter.toJson(deserialized)),
+                "Object should still be equal after serialization and deserialization.")
+    }
+
+    private data class TransientProperty(@Transient val nonBacking: String)
+
+    @Test(expected = IllegalArgumentException::class) fun testTransient() {
+        moshi.adapter(TransientProperty::class.java)
+    }
+
+    private data class NonTransientNonConstructorProperty(val ignored: String) {
+        var x: String = ""
+    }
+
+    @Test(expected = IllegalArgumentException::class) fun testNonTransientNonConstructorProperty() {
+        moshi.adapter(NonTransientNonConstructorProperty::class.java)
+    }
+
+    // def can be transient since a default value is provided
+    data class DefaultParam(val x: String, @Transient val def: String = "")
+
+    @Test fun testDefaultParam() {
+        val json = "{\"x\":\"x\"}"
+        val deserialized = moshi.adapter(DefaultParam::class.java).fromJson(json)
+        assertEquals("x", deserialized.x, "Existing value should be used from JSON.")
+        assertEquals("", deserialized.def, "Default value from constructor should be used.")
+    }
+
+    data class CrashOnMissing(val x: String = "")
+
+    @Test(expected = IllegalArgumentException::class) fun testCrashOnMissing() {
+        val json = "{}"
+        Moshi.Builder()
+                .add(KotlinJsonAdapterFactory(crashOnMissingValues = true))
+                .build()
+                .adapter(CrashOnMissing::class.java)
+                .fromJson(json)
+    }
+
+    @Test(expected = IllegalArgumentException::class) fun testMissingJsonValue() {
+        val json = "{}"
+        moshi.adapter(DefaultParam::class.java).fromJson(json)
+    }
+
+    data class Inner(val x: String)
+    data class Outer(val inner: Inner)
+
+    @Test fun testInnerOuter() {
+        val json = "{\"inner\": {\"x\": \"\"}}"
+        val deserialized = moshi.adapter(Outer::class.java).fromJson(json)
+        assertEquals("", deserialized.inner.x, "Nested objects should be deserialized corretly.")
+    }
+}
