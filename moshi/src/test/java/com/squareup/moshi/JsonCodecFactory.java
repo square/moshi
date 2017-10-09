0440283c0afd2495b98a803f48d7fc95a69331f9
Levente Farkas <lfarkas@lfarkas.org>
2017-10-09 17:46:21 +0200

    fix a few reasource leak and compile warning

--- moshi/src/test/java/com/squareup/moshi/JsonCodecFactory.java
+++ moshi/src/test/java/com/squareup/moshi/JsonCodecFactory.java
@@ -29,8 +29,10 @@ abstract class JsonCodecFactory {
       Buffer buffer;
 
       @Override public JsonReader newReader(String json) {
-        Buffer buffer = new Buffer().writeUtf8(json);
-        return JsonReader.of(buffer);
+        try (Buffer buffer = new Buffer()) {
+          buffer.writeUtf8(json);
+          return JsonReader.of(buffer);
+        }
       }
 
       @Override JsonWriter newWriter() {
