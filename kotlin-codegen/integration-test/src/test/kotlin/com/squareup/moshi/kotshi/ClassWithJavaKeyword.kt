266bceb981817ed84cc8e6d8c78c3a0fd4dffda1
Zac Sweers <zac.sweers@gmail.com>
2018-02-12 14:44:28 +0000

    Clean up a bunch of leftover comments

--- kotlin-codegen/integration-test/src/test/kotlin/com/squareup/moshi/kotshi/ClassWithJavaKeyword.kt
+++ kotlin-codegen/integration-test/src/test/kotlin/com/squareup/moshi/kotshi/ClassWithJavaKeyword.kt
@@ -5,13 +5,10 @@ import com.squareup.moshi.MoshiSerializable
 
 @MoshiSerializable
 data class ClassWithJavaKeyword(
-//        @GetterName("getDefault")
     @Json(name = "default")
     val default: Boolean,
-//        @GetterName("getInt")
     @Json(name = "int")
     val int: Int,
-//        @GetterName("someCase")
     @Json(name = "case")
     @get:JvmName("someCase")
     val case: Int
--- kotlin-codegen/integration-test/src/test/kotlin/com/squareup/moshi/kotshi/MyClass.kt
+++ kotlin-codegen/integration-test/src/test/kotlin/com/squareup/moshi/kotshi/MyClass.kt
@@ -2,24 +2,12 @@ package com.squareup.moshi.kotshi
 
 import com.squareup.moshi.MoshiSerializable
 
-//@JsonDefaultValue
 annotation class StringWithNA
 
 @MoshiSerializable
 data class MyClass(
-//        @JsonDefaultValue
-    val name: String,
+    val name: String = "",
     @StringWithNA
-    val address: String,
-//        @JsonDefaultValueInt(-1)
+    val address: String, // = "N/A",
     val age: Int
-) {
-  companion object {
-    //        @JsonDefaultValue
-    @JvmField
-    val defaultString = ""
-
-    @StringWithNA
-    fun defaultStringWithNA() = "N/A"
-  }
-}
+)
