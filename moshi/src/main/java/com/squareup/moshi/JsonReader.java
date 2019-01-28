19f215df1028f5922a4c6665ce5902f194f10f9e
thomas <thomasjsvos@gmail.com>
2019-01-28 14:12:39 +0100

    Fix typo in documentation for JsonReader.peekJson()

--- moshi/src/main/java/com/squareup/moshi/JsonReader.java
+++ moshi/src/main/java/com/squareup/moshi/JsonReader.java
@@ -478,7 +478,7 @@ public abstract class JsonReader implements Closeable {
    * Returns a new {@code JsonReader} that can read data from this {@code JsonReader} without
    * consuming it. The returned reader becomes invalid once this one is next read or closed.
    *
-   * <p>For example, we can use {@code peek()} to lookahead and read the same data multiple times.
+   * <p>For example, we can use {@code peekJson()} to lookahead and read the same data multiple times.
    *
    * <pre> {@code
    *
@@ -489,7 +489,7 @@ public abstract class JsonReader implements Closeable {
    *   jsonReader.beginArray();
    *   jsonReader.nextInt(); // Returns 123, reader contains 456, 789 and ].
    *
-   *   JsonReader peek = reader.peekReader();
+   *   JsonReader peek = reader.peekJson();
    *   peek.nextInt() // Returns 456.
    *   peek.nextInt() // Returns 789.
    *   peek.endArray()
