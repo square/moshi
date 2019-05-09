0404ab02cbc5e4d3af9a76198c40e2c7a83024cb
Zac Sweers <zac.sweers@gmail.com>
2019-05-08 19:05:44 -0700

    Allow no-moshi constructors for generated adapters

--- kotlin/tests/src/test/kotlin/com/squareup/moshi/kotlin/codgen/GeneratedAdaptersTest_CustomGeneratedClassJsonAdapter.kt
+++ kotlin/tests/src/test/kotlin/com/squareup/moshi/kotlin/codgen/GeneratedAdaptersTest_CustomGeneratedClassJsonAdapter.kt
@@ -3,10 +3,10 @@ package com.squareup.moshi.kotlin.codgen
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonReader
 import com.squareup.moshi.JsonWriter
-import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.codgen.GeneratedAdaptersTest.CustomGeneratedClass
 
-class GeneratedAdaptersTest_CustomGeneratedClassJsonAdapter(moshi: Moshi) : JsonAdapter<CustomGeneratedClass>() {
+// This also tests custom generated types with no moshi constructor
+class GeneratedAdaptersTest_CustomGeneratedClassJsonAdapter : JsonAdapter<CustomGeneratedClass>() {
   override fun fromJson(reader: JsonReader): CustomGeneratedClass? {
     TODO()
   }
