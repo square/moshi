36310c21ba4b2e44e56de563d867b48500301a29
Jin Kwon <onacit@gmail.com>
2019-06-19 11:47:55 +0900

    [#866] Add VoidAdapter

---
+++ adapters/src/main/java/com/squareup/moshi/adapters/VoidAdapter.java
@@ -0,0 +1,46 @@
+package com.squareup.moshi.adapters;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.JsonReader;
+import com.squareup.moshi.JsonWriter;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+
+/**
+ * An adapter for {@link Void}.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
+public final class VoidAdapter extends JsonAdapter<Void> {
+
+    /**
+     * Adapts from JSON using specified JSON reader. The {@code fromJson} method of {@code VoidAdapter} class invokes
+     * {@link JsonReader#skipValue()} on specified {@code reader} and always returns {@code null}.
+     *
+     * @param reader the JSON reader.
+     * @return {@code null}.
+     * @throws IOException if an I/O error occurs.
+     * @see JsonReader#skipValue()
+     */
+    @Nullable
+    @Override
+    public Void fromJson(final JsonReader reader) throws IOException {
+        reader.skipValue();
+        return null;
+    }
+
+    /**
+     * Adapts specified value to JSON using specified JSON writer. The {@code toJson} method of {@code VoidAdapter}
+     * invokes {@link JsonWriter#nullValue()} on specified {@code writer} and returns.
+     *
+     * @param writer the JSON writer.
+     * @param value  the value to adapt.
+     * @throws IOException if an I/O error occurs.
+     * @see JsonWriter#nullValue()
+     */
+    @Override
+    public void toJson(final JsonWriter writer, final @Nullable Void value) throws IOException {
+        writer.nullValue();
+    }
+}
