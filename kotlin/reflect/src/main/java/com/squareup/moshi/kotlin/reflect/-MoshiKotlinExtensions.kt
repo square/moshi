913d77487bad6a49fd41ced35122524c61458a32
Zac Sweers <zac.sweers@gmail.com>
2019-07-02 14:24:37 -0400

    Start MoshiKotlinExtensions

---
+++ kotlin/reflect/src/main/java/com/squareup/moshi/kotlin/reflect/-MoshiKotlinExtensions.kt
@@ -0,0 +1,21 @@
+package com.squareup.moshi.kotlin.reflect
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import kotlin.reflect.jvm.javaType
+import kotlin.reflect.typeOf
+
+/**
+ * @return a [JsonAdapter] for [T], creating it if necessary. Note that while nullability of [T]
+ *         itself is handled, nested types (such as in generics) are not resolved.
+ */
+@ExperimentalStdlibApi
+inline fun <reified T> Moshi.adapter(): JsonAdapter<T> {
+  val ktype = typeOf<T>()
+  val adapter = adapter<T>(ktype.javaType)
+  return if (ktype.isMarkedNullable) {
+    adapter.nullSafe()
+  } else {
+    adapter.nonNull()
+  }
+}
