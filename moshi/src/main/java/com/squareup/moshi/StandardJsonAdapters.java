a2ba75f27aa85b9095723b9f03dcb56a266d5a28
Zac Sweers <zsweers@slack-corp.com>
2020-01-11 17:18:09 -0500

    Imports

--- moshi/src/main/java/com/squareup/moshi/StandardJsonAdapters.java
+++ moshi/src/main/java/com/squareup/moshi/StandardJsonAdapters.java
@@ -24,9 +24,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import javax.annotation.Nullable;
-
-import static com.squareup.moshi.internal.Util.generatedAdapter;
 
 final class StandardJsonAdapters {
   private StandardJsonAdapters() {
