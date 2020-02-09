64515d5f4b0234e59f46c8c184e25292cbfce8ef
Rafael Oliveira <bellotti.rafael@gmail.com>
2020-02-08 16:19:34 -0800

    non null and null safe state changes

--- moshi/src/test/java/com/squareup/moshi/JsonAdapterStateTest.java
+++ moshi/src/test/java/com/squareup/moshi/JsonAdapterStateTest.java
@@ -1,11 +1,80 @@
-package com.squareup.moshi.internal;
+package com.squareup.moshi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
 
 public class JsonAdapterStateTest {
 
+    private Moshi moshi;
+
+    @BeforeEach
+    public void initializeBeforeEach() {
+        this.moshi = new Moshi.Builder().build();
+    }
+
     @Test
-    public void nonNullJsonAdapterTest() {
-        
+    public void nonNullJsonAdapterTest() throws IOException {
+        String json = "null";
+
+        JsonAdapter<Person> adapter = moshi.adapter(Person.class);
+        boolean exceptionThrown = false;
+        try {
+            adapter.nonNull().fromJson(json);
+        } catch (JsonDataException exception) {
+            exceptionThrown = true;
+        }
+
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void nullSafeJsonAdapterTest() throws IOException {
+        String json = "null";
+
+        JsonAdapter<Person> adapter = moshi.adapter(Person.class);
+        Person generatedPerson = adapter.nullSafe().fromJson(json);
+
+        assertNull(generatedPerson);
+    }
+
+    @Test
+    public void nullSafeToNullSafeJsonAdapterTest() throws IOException {
+        String json = "null";
+
+        JsonAdapter<Person> adapter = moshi.adapter(Person.class).nullSafe();
+
+        assertNull(adapter.fromJson(json));
+        assertNull(adapter.nullSafe().fromJson(json));
+    }
+
+    @Test
+    public void nullSafeToNonNullJsonAdapterTest() throws IOException {
+        String json = "null";
+
+        JsonAdapter<Person> adapter = moshi.adapter(Person.class).nullSafe();
+
+        assertNull(adapter.fromJson(json));
+        boolean exceptionThrown = false;
+        try {
+            adapter.nonNull().fromJson(json);
+        } catch (JsonDataException exception) {
+            exceptionThrown = true;
+        }
+
+        assertTrue(exceptionThrown);
+    }
+
+    public static class Person {
+        String name;
+        Integer age;
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
     }
 }
