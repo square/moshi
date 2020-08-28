701d6ba968b571bc872a04ae35afd062c523b7f8
Zac Sweers <zac.sweers@gmail.com>
2020-08-27 23:01:51 -0400

    Minor gradle cleanups (#1197)
    * Update to Gradle 6.6
    
    * Remove stdlib deps
    
    No longer necessary in Kotlin 1.4.0
    
    * Gradle 6.6.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -35,7 +35,6 @@ object Dependencies {
 
   object Kotlin {
     const val version = "1.4.0"
-    const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
     const val metadata = "org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.1.0"
   }
 
