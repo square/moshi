0b493b4450973e340a60d8ec0537a6c3e226734d
Zac Sweers <pandanomic@gmail.com>
2020-09-04 11:45:40 -0400

    Update to shadow 6.0

--- kotlin/codegen/build.gradle.kts
+++ kotlin/codegen/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
   kotlin("jvm")
   kotlin("kapt")
   id("com.vanniktech.maven.publish")
-  id("com.github.johnrengelman.shadow") version "5.2.0"
+  id("com.github.johnrengelman.shadow") version "6.0.0"
 }
 
 tasks.withType<KotlinCompile>().configureEach {
