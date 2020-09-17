a9f8450a3ba0e808d966e748a92bc527756535bb
Zac Sweers <pandanomic@gmail.com>
2020-09-17 16:32:50 -0400

    Add kotlin options

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -13,10 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm")
   kotlin("kapt")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+  kotlinOptions {
+    jvmTarget = "1.7"
+    freeCompilerArgs = listOf("-progressive")
+  }
 }
 
 dependencies {
