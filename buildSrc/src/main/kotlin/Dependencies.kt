51ec13dfe6b154ecefbf67c457c014548184ac65
Zac Sweers <pandanomic@gmail.com>
2020-09-26 16:21:40 -0400

    Update test dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -49,8 +49,8 @@ object Dependencies {
 
   object Testing {
     const val assertj = "org.assertj:assertj-core:3.11.1"
-    const val compileTesting = "com.github.tschuchortdev:kotlin-compile-testing:1.2.10"
-    const val junit = "junit:junit:4.12"
-    const val truth = "com.google.truth:truth:1.0"
+    const val compileTesting = "com.github.tschuchortdev:kotlin-compile-testing:1.2.11"
+    const val junit = "junit:junit:4.13"
+    const val truth = "com.google.truth:truth:1.0.1"
   }
 }
