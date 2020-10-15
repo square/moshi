0e21be07b4bf33cc356847199b977ecc86da0cee
Zac Sweers <pandanomic@gmail.com>
2020-10-15 01:25:11 -0400

    Junit 4.13.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -50,7 +50,7 @@ object Dependencies {
   object Testing {
     const val assertj = "org.assertj:assertj-core:3.11.1"
     const val compileTesting = "com.github.tschuchortdev:kotlin-compile-testing:1.2.11"
-    const val junit = "junit:junit:4.13"
+    const val junit = "junit:junit:4.13.1"
     const val truth = "com.google.truth:truth:1.0.1"
   }
 }
