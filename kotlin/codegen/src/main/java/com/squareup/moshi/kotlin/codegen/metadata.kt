23b3c6471f6072d126a14fba8ca0c3a8867452d4
Masatoshi Kubode <masatoshi@wantedly.com>
2021-01-14 15:52:45 +0900

    Use rawType().canonicalName instead of toString()

--- kotlin/codegen/src/main/java/com/squareup/moshi/kotlin/codegen/metadata.kt
+++ kotlin/codegen/src/main/java/com/squareup/moshi/kotlin/codegen/metadata.kt
@@ -483,7 +483,7 @@ internal fun TargetProperty.generator(
 private fun List<AnnotationSpec>?.qualifiers(elements: Elements): Set<AnnotationSpec> {
   if (this == null) return setOf()
   return filterTo(mutableSetOf()) {
-    elements.getTypeElement(it.typeName.toString())?.getAnnotation(JSON_QUALIFIER) != null
+    elements.getTypeElement(it.typeName.rawType().canonicalName)?.getAnnotation(JSON_QUALIFIER) != null
   }
 }
 
