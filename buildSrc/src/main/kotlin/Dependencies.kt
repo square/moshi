9272190d29552714c8a120d6f602b0d6e051f780
Zac Sweers <pandanomic@gmail.com>
2021-02-14 23:16:53 -0500

    Truth 1.1.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -55,6 +55,6 @@ object Dependencies {
     const val assertj = "org.assertj:assertj-core:3.11.1"
     const val compileTesting = "com.github.tschuchortdev:kotlin-compile-testing:1.3.4"
     const val junit = "junit:junit:4.13.1"
-    const val truth = "com.google.truth:truth:1.0.1"
+    const val truth = "com.google.truth:truth:1.1.2"
   }
 }
