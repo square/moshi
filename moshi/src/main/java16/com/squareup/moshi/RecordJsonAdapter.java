d98ee90ef547a0f96928262df05a161b2b6b3516
Zac Sweers <pandanomic@gmail.com>
2021-08-20 23:15:04 -0400

    GJF it properly

--- moshi/src/main/java16/com/squareup/moshi/RecordJsonAdapter.java
+++ moshi/src/main/java16/com/squareup/moshi/RecordJsonAdapter.java
@@ -32,162 +32,170 @@ import java.util.Set;
 
 /**
  * A {@link JsonAdapter} that supports Java {@code record} classes via reflection.
- * <p>
- * <em>NOTE:</em> Java records require JDK 16 or higher.
+ *
+ * <p><em>NOTE:</em> Java records require JDK 16 or higher.
  */
 final class RecordJsonAdapter<T> extends JsonAdapter<T> {
 
-  static final JsonAdapter.Factory FACTORY = (type, annotations, moshi) -> {
-    if (!annotations.isEmpty()) { return null; }
-
-    if (!(type instanceof Class) && !(type instanceof ParameterizedType)) {
-      return null;
-    }
-
-    var rawType = Types.getRawType(type);
-    if (!rawType.isRecord()) { return null; }
-
-    Map<String, Type> mappedTypeArgs = null;
-    if (type instanceof ParameterizedType parameterizedType) {
-      Type[] typeArgs = parameterizedType.getActualTypeArguments();
-      var typeVars = rawType.getTypeParameters();
-      mappedTypeArgs = new LinkedHashMap<>(typeArgs.length);
-      for (int i = 0; i < typeArgs.length; ++i) {
-        var typeVarName = typeVars[i].getName();
-        var materialized = typeArgs[i];
-        mappedTypeArgs.put(typeVarName, materialized);
-      }
-    }
-    var components = rawType.getRecordComponents();
-    var bindings = new LinkedHashMap<String, ComponentBinding<?>>();
-    var constructorParams = new Class<?>[components.length];
-    for (int i = 0, componentsLength = components.length; i < componentsLength; i++) {
-      RecordComponent component = components[i];
-      constructorParams[i] = component.getType();
-      var name = component.getName();
-      var componentType = component.getGenericType();
-      if (componentType instanceof TypeVariable<?> typeVariable) {
-        var typeVarName = typeVariable.getName();
-        if (mappedTypeArgs == null) {
-          throw new AssertionError("No mapped type arguments found for type '" + typeVarName + "'");
+  static final JsonAdapter.Factory FACTORY =
+      (type, annotations, moshi) -> {
+        if (!annotations.isEmpty()) {
+          return null;
         }
-        var mappedType = mappedTypeArgs.get(typeVarName);
-        if (mappedType == null) {
-          throw new AssertionError("No materialized type argument found for type '" + typeVarName + "'");
+
+        if (!(type instanceof Class) && !(type instanceof ParameterizedType)) {
+          return null;
         }
-        componentType = mappedType;
-      }
-      var jsonName = name;
-      Set<Annotation> qualifiers = null;
-      for (var annotation : component.getDeclaredAnnotations()) {
-        if (annotation instanceof Json jsonAnnotation) {
-          jsonName = jsonAnnotation.name();
-        } else {
-          if (annotation.annotationType().isAnnotationPresent(JsonQualifier.class)) {
-            if (qualifiers == null) {
-              qualifiers = new LinkedHashSet<>();
-            }
-            qualifiers.add(annotation);
+
+        var rawType = Types.getRawType(type);
+        if (!rawType.isRecord()) {
+          return null;
+        }
+
+        Map<String, Type> mappedTypeArgs = null;
+        if (type instanceof ParameterizedType parameterizedType) {
+          Type[] typeArgs = parameterizedType.getActualTypeArguments();
+          var typeVars = rawType.getTypeParameters();
+          mappedTypeArgs = new LinkedHashMap<>(typeArgs.length);
+          for (int i = 0; i < typeArgs.length; ++i) {
+            var typeVarName = typeVars[i].getName();
+            var materialized = typeArgs[i];
+            mappedTypeArgs.put(typeVarName, materialized);
           }
         }
-      }
-      if (qualifiers == null) {
-        qualifiers = Collections.emptySet();
-      }
-      var adapter = moshi.adapter(componentType, qualifiers);
-      var accessor = component.getAccessor();
-      accessor.setAccessible(true);
-      var componentBinding = new ComponentBinding<>(name, jsonName, adapter, accessor);
-      var replaced = bindings.put(jsonName, componentBinding);
-      if (replaced != null) {
-        throw new IllegalArgumentException(
-            "Conflicting components:\n"
-                + "    "
-                + replaced.name
-                + "\n"
-                + "    "
-                + componentBinding.name);
-      }
-    }
-    Constructor<Object> constructor;
-    try {
-      //noinspection unchecked
-      constructor = (Constructor<Object>) rawType.getDeclaredConstructor(constructorParams);
-      constructor.setAccessible(true);
-    } catch (NoSuchMethodException e) {
-      throw new AssertionError(e);
-    }
-    return new RecordJsonAdapter<>(constructor, bindings).nullSafe();
-  };
-
-  private static record ComponentBinding<T>(String name, String jsonName, JsonAdapter<T> adapter, Method accessor) {
-  }
-
-    private final String targetClass;
-    private final Constructor<T> constructor;
-    private final ComponentBinding<Object>[] componentBindingsArray;
-    private final JsonReader.Options options;
-
-    @SuppressWarnings("ToArrayCallWithZeroLengthArrayArgument")
-    public RecordJsonAdapter(
-        Constructor<T> constructor,
-        Map<String, ComponentBinding<?>> componentBindings
-    ) {
-      this.constructor = constructor;
-      this.targetClass = constructor.getDeclaringClass().getSimpleName();
-      //noinspection unchecked
-      this.componentBindingsArray = componentBindings.values().toArray(new ComponentBinding[componentBindings.size()]);
-      this.options = JsonReader.Options.of(componentBindings.keySet().toArray(new String[componentBindings.size()]));
-    }
-
-    @Override
-    public T fromJson(JsonReader reader) throws IOException {
-      var resultsArray = new Object[componentBindingsArray.length];
-
-      reader.beginObject();
-      while (reader.hasNext()) {
-        int index = reader.selectName(options);
-        if (index == -1) {
-          reader.skipName();
-          reader.skipValue();
-          continue;
+        var components = rawType.getRecordComponents();
+        var bindings = new LinkedHashMap<String, ComponentBinding<?>>();
+        var constructorParams = new Class<?>[components.length];
+        for (int i = 0, componentsLength = components.length; i < componentsLength; i++) {
+          RecordComponent component = components[i];
+          constructorParams[i] = component.getType();
+          var name = component.getName();
+          var componentType = component.getGenericType();
+          if (componentType instanceof TypeVariable<?> typeVariable) {
+            var typeVarName = typeVariable.getName();
+            if (mappedTypeArgs == null) {
+              throw new AssertionError(
+                  "No mapped type arguments found for type '" + typeVarName + "'");
+            }
+            var mappedType = mappedTypeArgs.get(typeVarName);
+            if (mappedType == null) {
+              throw new AssertionError(
+                  "No materialized type argument found for type '" + typeVarName + "'");
+            }
+            componentType = mappedType;
+          }
+          var jsonName = name;
+          Set<Annotation> qualifiers = null;
+          for (var annotation : component.getDeclaredAnnotations()) {
+            if (annotation instanceof Json jsonAnnotation) {
+              jsonName = jsonAnnotation.name();
+            } else {
+              if (annotation.annotationType().isAnnotationPresent(JsonQualifier.class)) {
+                if (qualifiers == null) {
+                  qualifiers = new LinkedHashSet<>();
+                }
+                qualifiers.add(annotation);
+              }
+            }
+          }
+          if (qualifiers == null) {
+            qualifiers = Collections.emptySet();
+          }
+          var adapter = moshi.adapter(componentType, qualifiers);
+          var accessor = component.getAccessor();
+          accessor.setAccessible(true);
+          var componentBinding = new ComponentBinding<>(name, jsonName, adapter, accessor);
+          var replaced = bindings.put(jsonName, componentBinding);
+          if (replaced != null) {
+            throw new IllegalArgumentException(
+                "Conflicting components:\n"
+                    + "    "
+                    + replaced.name
+                    + "\n"
+                    + "    "
+                    + componentBinding.name);
+          }
         }
-        var result = componentBindingsArray[index].adapter.fromJson(reader);
-        resultsArray[index] = result;
-      }
-      reader.endObject();
-
-      var initialAccess = constructor.canAccess(null);
-      try {
-        if (!initialAccess) {
-          constructor.setAccessible(true);
-        }
-        return constructor.newInstance(resultsArray);
-      } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-        throw new AssertionError(e);
-      } finally {
-        constructor.setAccessible(false);
-      }
-    }
-
-    @Override
-    public void toJson(JsonWriter writer, T value) throws IOException {
-      writer.beginObject();
-
-      for (var binding : componentBindingsArray) {
-        writer.name(binding.jsonName);
+        Constructor<Object> constructor;
         try {
-          binding.adapter.toJson(writer, binding.accessor.invoke(value));
-        } catch (IllegalAccessException | InvocationTargetException e) {
+          //noinspection unchecked
+          constructor = (Constructor<Object>) rawType.getDeclaredConstructor(constructorParams);
+          constructor.setAccessible(true);
+        } catch (NoSuchMethodException e) {
           throw new AssertionError(e);
         }
+        return new RecordJsonAdapter<>(constructor, bindings).nullSafe();
+      };
+
+  private static record ComponentBinding<T>(
+      String name, String jsonName, JsonAdapter<T> adapter, Method accessor) {}
+
+  private final String targetClass;
+  private final Constructor<T> constructor;
+  private final ComponentBinding<Object>[] componentBindingsArray;
+  private final JsonReader.Options options;
+
+  @SuppressWarnings("ToArrayCallWithZeroLengthArrayArgument")
+  public RecordJsonAdapter(
+      Constructor<T> constructor, Map<String, ComponentBinding<?>> componentBindings) {
+    this.constructor = constructor;
+    this.targetClass = constructor.getDeclaringClass().getSimpleName();
+    //noinspection unchecked
+    this.componentBindingsArray =
+        componentBindings.values().toArray(new ComponentBinding[componentBindings.size()]);
+    this.options =
+        JsonReader.Options.of(
+            componentBindings.keySet().toArray(new String[componentBindings.size()]));
+  }
+
+  @Override
+  public T fromJson(JsonReader reader) throws IOException {
+    var resultsArray = new Object[componentBindingsArray.length];
+
+    reader.beginObject();
+    while (reader.hasNext()) {
+      int index = reader.selectName(options);
+      if (index == -1) {
+        reader.skipName();
+        reader.skipValue();
+        continue;
       }
+      var result = componentBindingsArray[index].adapter.fromJson(reader);
+      resultsArray[index] = result;
+    }
+    reader.endObject();
 
-      writer.endObject();
+    var initialAccess = constructor.canAccess(null);
+    try {
+      if (!initialAccess) {
+        constructor.setAccessible(true);
+      }
+      return constructor.newInstance(resultsArray);
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+      throw new AssertionError(e);
+    } finally {
+      constructor.setAccessible(false);
+    }
+  }
+
+  @Override
+  public void toJson(JsonWriter writer, T value) throws IOException {
+    writer.beginObject();
+
+    for (var binding : componentBindingsArray) {
+      writer.name(binding.jsonName);
+      try {
+        binding.adapter.toJson(writer, binding.accessor.invoke(value));
+      } catch (IllegalAccessException | InvocationTargetException e) {
+        throw new AssertionError(e);
+      }
     }
 
-    @Override
-    public String toString() {
-      return "JsonAdapter(" + targetClass + ")";
-    }
+    writer.endObject();
+  }
+
+  @Override
+  public String toString() {
+    return "JsonAdapter(" + targetClass + ")";
+  }
 }
