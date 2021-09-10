66a3933cdf49ce81a80cf3f41b0cacbba01c8579
Zac Sweers <pandanomic@gmail.com>
2021-09-09 22:16:43 -0400

    Remove kotlin toolchain debugging

--- kotlin/tests/build.gradle.kts
+++ kotlin/tests/build.gradle.kts
@@ -41,21 +41,6 @@ tasks.withType<KotlinCompile>().configureEach {
       "-Werror",
       "-Xopt-in=kotlin.ExperimentalStdlibApi"
     )
-
-    println("Adding args to $name")
-    kotlinJavaToolchain.toolchain.use(javaToolchains.launcherFor { languageVersion.set(JavaLanguageVersion.of(11)) })
-    kotlinDaemonJvmArguments.addAll(
-      "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
-      "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
-      "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
-      "--add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
-      "--add-opens=jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED",
-      "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
-      "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
-      "--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
-      "--add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
-      "--add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
-    )
   }
 }
 
