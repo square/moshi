862ff7ddcd3ff5805e6bd15966113b851d251886
Zac Sweers <zac.sweers@gmail.com>
2021-10-16 02:18:51 -0400

    Update to JDK 17 (#1394)
    Everything still targets JDK 8 (non-records) or JDK 16 (records only)

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,7 +112,7 @@ subprojects {
   pluginManager.withPlugin("java") {
     configure<JavaPluginExtension> {
       toolchain {
-        languageVersion.set(JavaLanguageVersion.of(16))
+        languageVersion.set(JavaLanguageVersion.of(17))
       }
     }
     if (project.name != "records-tests") {
--- moshi/records-tests/build.gradle.kts
+++ moshi/records-tests/build.gradle.kts
@@ -18,7 +18,14 @@ plugins {
   `java-library`
 }
 
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+  }
+}
+
 tasks.withType<JavaCompile>().configureEach {
+  // Target 16 to ensure minimum compatibility
   options.release.set(16)
 }
 
