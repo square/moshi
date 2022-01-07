8a1128b8e9b7408377654ee81f1994517a487f50
Zac Sweers <pandanomic@gmail.com>
2022-01-07 13:12:59 -0500

    Add missing empty constructor

--- moshi/src/main/java/com/squareup/moshi/JsonDataException.kt
+++ moshi/src/main/java/com/squareup/moshi/JsonDataException.kt
@@ -28,6 +28,7 @@ package com.squareup.moshi
  * [StackOverflowError].
  */
 public class JsonDataException : RuntimeException {
+  public constructor() : super()
   public constructor(message: String?) : super(message)
   public constructor(cause: Throwable?) : super(cause)
   public constructor(message: String?, cause: Throwable?) : super(message, cause)
