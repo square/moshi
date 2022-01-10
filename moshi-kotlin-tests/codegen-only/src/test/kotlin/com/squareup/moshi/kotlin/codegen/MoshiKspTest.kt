ed5c49ce9e7928128488c2be4688503e1873d1c0
Zac Sweers <pandanomic@gmail.com>
2022-01-10 11:42:49 -0500

    Remove now-redundant imports

--- moshi-kotlin-tests/codegen-only/src/test/kotlin/com/squareup/moshi/kotlin/codegen/MoshiKspTest.kt
+++ moshi-kotlin-tests/codegen-only/src/test/kotlin/com/squareup/moshi/kotlin/codegen/MoshiKspTest.kt
@@ -18,7 +18,6 @@ package com.squareup.moshi.kotlin.codegen
 import com.google.common.truth.Truth.assertThat
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.adapter
 import org.junit.Test
 
 // Regression tests specific to Moshi-KSP
