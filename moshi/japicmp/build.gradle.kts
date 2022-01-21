b2a67760d42a6f89d8931433252fc3df233b132c
Zac Sweers <zac.sweers@gmail.com>
2022-01-21 17:22:13 -0500

    Explicitly denote util public APIs in japicmp (#1511)

--- moshi/japicmp/build.gradle.kts
+++ moshi/japicmp/build.gradle.kts
@@ -26,16 +26,25 @@ val japicmp = tasks.register<JapicmpTask>("japicmp") {
   isIgnoreMissingClasses = true
   isIncludeSynthetic = true
   classExcludes = listOf(
-    "com.squareup.moshi.internal.NonNullJsonAdapter", // Internal.
-    "com.squareup.moshi.internal.NullSafeJsonAdapter", // Internal.
-    "com.squareup.moshi.internal.Util", // Internal.
-    "com.squareup.moshi.StandardJsonAdapters", // Package-private
-    "com.squareup.moshi.RecordJsonAdapter\$ComponentBinding", // Package-private
     "com.squareup.moshi.AdapterMethodsFactory", // Internal.
     "com.squareup.moshi.ClassJsonAdapter", // Internal.
+    "com.squareup.moshi.RecordJsonAdapter\$ComponentBinding", // Package-private
+    "com.squareup.moshi.StandardJsonAdapters", // Package-private
+    "com.squareup.moshi.internal.NonNullJsonAdapter", // Internal.
+    "com.squareup.moshi.internal.NullSafeJsonAdapter", // Internal.
+    "com.squareup.moshi.internal.Util\$GenericArrayTypeImpl", // Internal.
+    "com.squareup.moshi.internal.Util\$ParameterizedTypeImpl", // Internal.
+    "com.squareup.moshi.internal.Util\$WildcardTypeImpl", // Internal.
   )
   methodExcludes = listOf(
     "com.squareup.moshi.JsonAdapter#indent(java.lang.String)", // Was unintentionally open before
+    "com.squareup.moshi.internal.Util#hasNullable(java.lang.annotation.Annotation[])",
+    "com.squareup.moshi.internal.Util#jsonAnnotations(java.lang.annotation.Annotation[])",
+    "com.squareup.moshi.internal.Util#jsonAnnotations(java.lang.reflect.AnnotatedElement)",
+    "com.squareup.moshi.internal.Util#jsonName(java.lang.String, com.squareup.moshi.Json)",
+    "com.squareup.moshi.internal.Util#jsonName(java.lang.String, java.lang.reflect.AnnotatedElement)",
+    "com.squareup.moshi.internal.Util#resolve(java.lang.reflect.Type, java.lang.Class, java.lang.reflect.Type)",
+    "com.squareup.moshi.internal.Util#typeAnnotatedWithAnnotations(java.lang.reflect.Type, java.util.Set)",
   )
   fieldExcludes = listOf(
     "com.squareup.moshi.CollectionJsonAdapter#FACTORY", // False-positive, class is not public anyway
