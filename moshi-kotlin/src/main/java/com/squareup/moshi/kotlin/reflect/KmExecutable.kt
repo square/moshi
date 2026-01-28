c1575b12e06bf02f75ca3eb9d9b53f873691ea26
Zac Sweers <pandanomic@gmail.com>
2026-01-28 15:25:45 -0500

    Extract Invokable helper for backward compat
    Port of https://github.com/ZacSweers/MoshiX/pull/847

--- moshi-kotlin/src/main/java/com/squareup/moshi/kotlin/reflect/KmExecutable.kt
+++ moshi-kotlin/src/main/java/com/squareup/moshi/kotlin/reflect/KmExecutable.kt
@@ -1,8 +1,6 @@
 package com.squareup.moshi.kotlin.reflect
 
-import com.squareup.moshi.internal.DEFAULT_CONSTRUCTOR_MARKER
 import java.lang.reflect.Constructor
-import java.lang.reflect.Executable
 import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 import kotlin.metadata.KmClass
@@ -12,14 +10,11 @@ import kotlin.metadata.isValue
 import kotlin.metadata.jvm.KotlinClassMetadata
 import kotlin.metadata.jvm.signature
 
-private val DEFAULT_CONSTRUCTOR_SIGNATURE by
-  lazy(LazyThreadSafetyMode.NONE) { DEFAULT_CONSTRUCTOR_MARKER!!.descriptor }
-
 /**
  * Simple facade over KM constructor-ish types, which could be a constructor or a static creator
  * method (value classes).
  */
-internal sealed class KmExecutable<T : Executable> {
+internal sealed class KmExecutable<T : Any> {
   abstract val parameters: List<KtParameter>
   abstract val isDefault: Boolean
   abstract val needsDefaultMarker: Boolean
@@ -72,56 +67,25 @@ internal sealed class KmExecutable<T : Executable> {
       return boxImplMethod to unboxImplMethod
     }
 
-    private fun Executable.defaultsSignature(): String {
-      val suffixDescriptor =
-        when (this) {
-          is Constructor<*> -> "V"
-          is Method -> returnType.descriptor
-        }
-      val rawPrefix = jvmMethodSignature.removeSuffix(suffixDescriptor).removeSuffix(")")
-      val prefix =
-        when (this) {
-          is Constructor<*> -> {
-            rawPrefix
-          }
-
-          is Method -> {
-            // Need to add $default to the end of the method name
-            val (name, rest) = rawPrefix.split("(", limit = 2)
-            // ktlint doesn't support multi-dollar prefixes
-            @Suppress("CanConvertToMultiDollarString") "$name\$default($rest"
-          }
-        }
-      val parameterCount = parameterTypes.size
-      val maskParamsToAdd = (parameterCount + 31) / 32
-      val defaultConstructorSignature = buildString {
-        append(prefix)
-        repeat(maskParamsToAdd) { append("I") }
-        append(DEFAULT_CONSTRUCTOR_SIGNATURE)
-        append(')')
-        append(suffixDescriptor)
-      }
-      return defaultConstructorSignature
-    }
-
     operator fun invoke(rawType: Class<*>, kmClass: KmClass): KmExecutable<*>? {
       // If this is a value class, the "constructor" will actually be a static creator function
-      val constructorsBySignature =
+      val constructorsBySignature: Map<String, Invokable> =
         if (kmClass.isValue) {
           // kmConstructorSignature is something like constructor-impl(I)I
           // Need to look up the matching static function
           rawType.declaredMethods
             .filter { Modifier.isStatic(it.modifiers) }
-            .associateBy { it.jvmMethodSignature }
+            .associateBy({ it.jvmMethodSignature }, { Invokable.InvokableMethod(it) })
         } else {
-          rawType.declaredConstructors.associateBy { it.jvmMethodSignature }
+          rawType.declaredConstructors.associateBy(
+            { it.jvmMethodSignature },
+            { Invokable.InvokableConstructor(it) },
+          )
         }
       val kmConstructor = kmClass.constructors.find { !it.isSecondary } ?: return null
       val kmConstructorSignature = kmConstructor.signature?.toString() ?: return null
       val jvmConstructor = constructorsBySignature[kmConstructorSignature] ?: return null
 
-      val parameterAnnotations = jvmConstructor.parameterAnnotations
-      val parameterTypes = jvmConstructor.parameterTypes
       val parameters =
         kmConstructor.valueParameters.withIndex().map { (index, kmParam) ->
           // Check if this parameter's type is a value class
@@ -130,8 +94,8 @@ internal sealed class KmExecutable<T : Executable> {
           KtParameter(
             kmParam,
             index,
-            parameterTypes[index],
-            parameterAnnotations[index].toList(),
+            jvmConstructor.parameterTypes[index],
+            jvmConstructor.parameterAnnotations[index].toList(),
             parameterValueClassBoxer,
             parameterValueClassUnboxer,
           )
@@ -139,32 +103,36 @@ internal sealed class KmExecutable<T : Executable> {
       val anyOptional = parameters.any { it.declaresDefaultValue }
       val actualConstructor =
         if (anyOptional) {
-          val defaultsSignature = jvmConstructor.defaultsSignature()
-          constructorsBySignature[defaultsSignature] ?: return null
+          constructorsBySignature[jvmConstructor.defaultsSignature()] ?: return null
         } else {
           jvmConstructor
         }
 
-      actualConstructor.isAccessible = true
+      actualConstructor.setAccessible()
 
       return if (kmClass.isValue) {
         // Things get quirky here. KM will return the primary constructor for the value class
         // as a constructor-impl static function, BUT this function always only returns the
-        // underlying
-        // type. What we want is the boxed type, so we need to be able to invoke the constructor and
-        // then be able to pass it on to the box-impl function to get the full instance.
-        // We can't just skip ahead and use only the box-impl function because the constructor is
-        // the only one that handles default values
+        // underlying type. What we want is the boxed type, so we need to be able to invoke the
+        // constructor and then be able to pass it on to the box-impl function to get the full
+        // instance. We can't just skip ahead and use only the box-impl function because the
+        // constructor is the only one that handles default values
 
-        val boxImpl = constructorsBySignature.entries.first { it.key.startsWith("box-impl") }.value
+        val boxImpl =
+          constructorsBySignature.entries.first { it.key.startsWith("box-impl") }.value
+            as Invokable.InvokableMethod
         KmExecutableFunction(
-          actualConstructor as Method,
+          (actualConstructor as Invokable.InvokableMethod).method,
           parameters,
           anyOptional,
-          boxImpl as Method,
+          boxImpl.method,
         )
       } else {
-        KmExecutableConstructor(actualConstructor as Constructor<*>, parameters, anyOptional)
+        KmExecutableConstructor(
+          (actualConstructor as Invokable.InvokableConstructor).constructor,
+          parameters,
+          anyOptional,
+        )
       }
     }
   }
